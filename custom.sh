5e65cd5eaf04866f401ac5b9e2c23320a469570e
nam000termux <77914563+nam000termux@users.noreply.github.com>
2022-05-23 22:26:43 +0700

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,26 +1,25 @@
-read -sp 'Your Token Ngrok: ' token
-read -sp 'Your Custom Iso: ' iso
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken $token
+./ngrok authtoken 29Y7qXwzz01LI5RKuUvQgs7isKx_UWgYUeWwLZrLLZdYDTx1
 nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
-qemu-img create -f raw MeMay.img 200G
+qemu-img create -f raw windows7nano.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O iso.iso $iso
+wget -O windows7nano.iso 'https://dl.malwarewatch.org/windows/Windows%207%20%28SuperNano%29.iso'
+
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=iso.iso,media=cdrom \
-  -drive file=MeMay.img,format=raw,if=virtio \
+  -drive file=windows7nano.iso,media=cdrom \
+  -drive file=windows7nano.img,format=raw,if=virtio \
   -drive file=virtio-win.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
   -smp cores=2 \
-
