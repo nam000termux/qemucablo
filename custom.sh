396d658a9ef12022cdaab7192b11c497a610bf27
nam000termux <77914563+nam000termux@users.noreply.github.com>
2022-05-24 20:30:23 +0700

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,13 +1,11 @@
-read -p "Your Custom Iso:" iso
 sudo apt-get update
 apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw memay.img 1T
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O deptrai.iso $iso
-
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+clear
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
@@ -18,5 +16,5 @@ sudo qemu-system-x86_64 \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -device e1000
+  -device e1000 \
   -smp cores=2 \
